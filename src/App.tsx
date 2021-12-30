1ba2227e5f82534f1841d7621513f3e596c6f5c6
HeeYoung-Youn <yhy1@naver.com>
2021-12-30 10:09:15 +0900

    Add: MyForm Component
    MyForm Component 추가 / onChange, handleSubmit 시 event 객체의 정확한 type 명시

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Greetings from "./Greetings";
+import MyForm from "./MyForm";
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(name);
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
   };
-  return <Greetings name="React" onClick={onClick} />;
+
+  return <MyForm onSubmit={onSubmit} />;
 };
 
 export default App;
